a4e0138575ae9fee3b9b4b43e21b99039f24e650
catherine12345 <jy419@uw.edu>
2024-05-03 22:54:13 -0400

    CRUD

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Form, json, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
+import prisma from "~/lib/prisma";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +10,80 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function action({ request }) {
+  const formData = await request.formData();
+  if (request.method === "POST") {
+    const message = formData.get("message");
+
+    await prisma.message.create({
+      data: {
+        message: message
+      }
+    });
+
+    return json({ message: "Successfully sent message" })
+  } else if (request.method === "PATCH") {
+    const messageId = formData.get("messageId");
+    const message = formData.get("message");
+
+    await prisma.message.update({
+      where: {
+        id: messageId
+      },
+      data: {
+        message
+      }
+    })
+
+    return json({message: "Updated successfully"})
+  } else if (request.method === "DELETE") {
+    const messageId = formData.get("messageId");
+    await prisma.message.delete({
+      where: {
+        id: messageId
+      }
+    });
+
+    return json({message: "Deleted successfully"})
+  }
+}
+
 export default function Index() {
+  const data = useLoaderData();
+
+  const [editFocus, setEditFocus] = useState("");
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
+      <Form method="post">
+        <input type="text" name="message" id="message" />
+        <button type="submit">Send</button>
+      </Form>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {data.messages.map((message) => (
+          <li>
+            {message.message}<br />
+            {editFocus !== message.id && <button onClick={() => setEditFocus(message.id)}>Edit</button>}
+            {editFocus === message.id && (
+              <Form method="patch">
+                <input type="hidden" name="messageId" value={message.id} />
+                <input type="text" name="message" id="message" defaultValue={message.message} />
+                <button type="submit">Edit</button>
+              </Form>
+            )}
+            <Form method="delete">
+              <input type="hidden" name="messageId" value={message.id} />
+              <button type="submit">Delete</button>
+            </Form>
+          </li>
+        ))}
       </ul>
     </div>
   );
+}
+
+export async function loader({ request }) {
+  const messages = await prisma.message.findMany();
+
+  return json({ messages });
 }
